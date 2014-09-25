7ac6d81ab950ac3788df922fed2e0dda564d14ca
Gary Teh <garyjob@gmail.com>
2014-09-25 15:51:17 -0700

    reading and updating

--- test.js
+++
@@ -1,7 +0,0 @@
-var gpio = require("pi-gpio");
-
-gpio.open(1, "output", function(err) {     // Open pin 16 for output
-    gpio.write(1, 1, function() {          // Set pin 16 high (1)
-        gpio.close(1);                     // Close pin 16
-    });
-});
