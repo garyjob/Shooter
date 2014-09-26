ad908c1882fe24acac34104a291bcc591cefdc99
Gary Teh <garyjob@gmail.com>
2014-09-26 03:34:27 -0700

    version 1

---
+++ public/js/nerve_gun.js
@@ -0,0 +1,76 @@
+var defenceSystem = function() {
+  var self = this;
+  self.setControls();
+  self.connect();
+}
+
+defenceSystem.prototype.setControls = function() {
+  var self            = this;
+  self.log_query      = "#log";  
+  self.trigger_button = "#da_boomz";  
+
+  $(self.trigger_button).click(function() {
+    self.fire();
+  });
+
+  self.log({
+    status: 'SUCCESS',
+    message: 'Controls established'
+  });  
+
+}
+
+defenceSystem.prototype.connect = function() {
+  var self = this;
+  self.log({
+    status: 'PENDING',
+    message: 'Establishing uplink to Defence System'
+  });
+
+  self.socket = io.connect('http://localhost');
+
+  self.socket.on('connected', function (data) {
+    self.log(data);
+    self.enableTrigger();
+  });
+
+  self.socket.on('fired', function (data) {
+    self.log(data);
+    self.enableTrigger();
+  });  
+}
+
+defenceSystem.prototype.enableTrigger = function() {
+  var self = this;
+  $(self.trigger_button).removeAttr('disabled')
+}
+
+defenceSystem.prototype.disableTrigger = function() {
+  var self = this;
+  $(self.trigger_button).addAttr('disabled');
+}
+
+defenceSystem.prototype.fire = function() {
+  var self = this;
+
+  self.log({
+    status: 'PENDING',
+    message: 'Missle launch detected'
+  });
+
+  self.socket.emit("fire");
+}
+
+defenceSystem.prototype.log = function(msg_obj) {
+  console.log(msg_obj);
+  var self = this;
+
+  $log_entry = $("<div>");
+  $log_entry.html( msg_obj.status + " : " + msg_obj.message );
+
+  $log_wrapper = $(self.log_query);
+  $log_wrapper.prepend($log_entry);
+
+}
+
+var ds = new defenceSystem();
---
+++ server.coffee
@@ -0,0 +1,63 @@
+bodyParser = require('body-parser')
+express    = require 'express'
+path       = require 'path'
+fs         = require 'fs'
+nerve_gun  = require 'nerve_gun'
+
+global.CONFIG = null
+CONFIG = JSON.parse(fs.readFileSync(__dirname + '/config/config.js').toString());
+
+
+# Web Server section of system
+app = express()
+
+app.use bodyParser.urlencoded({ extended: false })
+app.use bodyParser.json()
+
+app.set 'views', __dirname + '/views'
+app.set 'view engine', 'ejs'
+app.use express.static(__dirname + '/public')
+
+
+app.get '/', (req, res)->
+  res.render 'index'
+
+
+
+# Socket IO server
+server  = require('http').Server(app)
+io      = require('socket.io')(server)
+
+# Start socket server
+io.on 'connection', (socket)->
+
+  socket.emit 'connected', { 
+    status: 'SUCCESS',
+    message: 'Defense System Armed'
+  }
+
+  socket.on 'fire', (pin_num)=>
+    console.log 'Boomz!!! Missiles fired...'
+    ng = new nerveGun CONFIG.pin_num
+    ng.fire()
+
+    socket.emit 'fired', { 
+      status: 'SUCCESS',
+      message: 'Boomz!!! Missiles fired...'
+    }
+
+  socket.on 'disconnect', ()=>
+    console.log "Opps... Captain America has went on holidays"
+
+
+
+module.exports = 
+  server : server
+
+
+
+if !module.parent
+  # Start tutorial server
+  port = process.argv[2] || CONFIG.port
+  server.listen port
+  console.log "Attack Server listening at port : %s", port
